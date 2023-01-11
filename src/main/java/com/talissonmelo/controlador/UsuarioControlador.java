66d11c35d110ea40e5f261fc67093fd1076e15e2
TalissonMelo <talissonmelorodrigues@gmail.com>
2023-01-11 16:50:08 -0300

    Efetuando listagem de usuários

--- src/main/java/com/talissonmelo/controlador/UsuarioControlador.java
+++ src/main/java/com/talissonmelo/controlador/UsuarioControlador.java
@@ -2,12 +2,10 @@ package com.talissonmelo.controlador;
 
 import com.talissonmelo.modelo.Usuario;
 import com.talissonmelo.modelo.dto.UsuarioDto;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -23,5 +21,11 @@ public class UsuarioControlador {
         usuario.criarUsuario(usuarioDto.getNome(), usuarioDto.getIdade());
         usuario.persist();
         return Response.ok(usuario).build();
+    }
+
+    @GET
+    public Response listar() {
+        PanacheQuery<Usuario> usuarios = Usuario.findAll();
+        return Response.ok(usuarios.list()).build();
     }
 }
--- src/main/java/com/talissonmelo/modelo/Usuario.java
+++ src/main/java/com/talissonmelo/modelo/Usuario.java
@@ -1,6 +1,5 @@
 package com.talissonmelo.modelo;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.*;
