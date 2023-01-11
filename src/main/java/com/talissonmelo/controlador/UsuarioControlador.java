02653c9faf67d3a30033c24c0856360912acc395
TalissonMelo <talissonmelorodrigues@gmail.com>
2023-01-11 17:16:33 -0300

    Concluindo as operações CRUD

--- src/main/java/com/talissonmelo/controlador/UsuarioControlador.java
+++ src/main/java/com/talissonmelo/controlador/UsuarioControlador.java
@@ -28,4 +28,33 @@ public class UsuarioControlador {
         PanacheQuery<Usuario> usuarios = Usuario.findAll();
         return Response.ok(usuarios.list()).build();
     }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deletar(@PathParam("id") Long id) {
+        Usuario usuario = Usuario.findById(id);
+        if(usuario != null) {
+            usuario.delete();
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response atualizar(@PathParam("id") Long id, UsuarioDto usuarioDto) {
+        Usuario usuario = Usuario.findById(id);
+        if(usuario != null) {
+            usuario.setIdade(usuarioDto.getIdade());
+            usuario.setNome(usuarioDto.getNome());
+            usuario.persist();
+            return Response.ok(usuario).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+
 }
